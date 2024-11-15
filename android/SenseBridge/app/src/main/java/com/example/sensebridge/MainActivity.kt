2a2bd38fc197f4179345ebfbfd5d9a73a27db3c7
Das, Swarnava <swarnava@qti.qualcomm.com>
2024-11-15 13:10:42 +0530

    Default home-screen structure

--- android/SenseBridge/app/src/main/java/com/example/sensebridge/MainActivity.kt
+++ android/SenseBridge/app/src/main/java/com/example/sensebridge/MainActivity.kt
@@ -4,47 +4,57 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.sensebridge.ui.theme.SenseBridgeTheme
 
 class MainActivity : ComponentActivity()
 {
-    companion object {
-        init {
-            System.loadLibrary("sensebridge_cpp_v1")
-            System.loadLibrary("sensebridge_cpp_v2")
-        }
-    }
-
     private external fun stringFromJNI(): String
     private external fun stringFromJNI2(): String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        System.loadLibrary("sensebridge_cpp_v1")
+        System.loadLibrary("sensebridge_cpp_v2")
         enableEdgeToEdge()
         setContent {
-            SenseBridgeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            // Text("${stringFromJNI()} ${stringFromJNI2()}!", modifier = Modifier.padding(50.dp))
+
+            HomeScreenUI()
         }
     }
 
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "${stringFromJNI()} ${stringFromJNI2()} Hello $name!",
-            modifier = modifier
-        )
+    fun HomeScreenUI()
+    {
+        Scaffold(topBar = { TopBar("Home") }) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            )
+            {
+                Text(text = "$innerPadding ${stringFromJNI()} ${stringFromJNI2()}!", modifier = Modifier.padding(16.dp))
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TopBar(text: String = "Top Bar")
+    {
+        TopAppBar(colors = topAppBarColors(containerColor = Color.Blue),
+            title = {Text(text, color = Color.White)})
     }
 }
